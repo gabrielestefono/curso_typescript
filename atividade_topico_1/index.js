894cde9e4ab4396b0f6bffb4931be0f1825259f0
gabrielestefono <gabrielestefono@hotmail.com>
2023-11-11 13:55:24 -0300

    Concluindo desafio tópico 01

---
+++ atividade_topico_1/index.js
@@ -0,0 +1,4 @@
+function somar(numero_1, numero_2) {
+    return numero_1 + numero_2;
+}
+console.log(somar(1, 2));
---
+++ atividade_topico_1/index.ts
@@ -0,0 +1,5 @@
+function somar(numero_1: number, numero_2: number){
+	return numero_1 + numero_2;
+}
+
+console.log(somar(1,2))
