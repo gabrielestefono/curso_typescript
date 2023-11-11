d33373e52d27cc0667730e67eec836157c94bf21
gabrielestefono <gabrielestefono@hotmail.com>
2023-11-11 13:48:30 -0300

    Iniciando Curso

---
+++ topico_1/index.js
@@ -0,0 +1,9 @@
+var firstname = 'Gabriel';
+var x = true;
+var anotherName = 1;
+function greeting(name) {
+    console.log('Olá, ' + name);
+}
+greeting(firstname);
+// greeting(x);
+// greeting(anotherName);
---
+++ topico_1/index.ts
@@ -0,0 +1,9 @@
+const firstname = 'Gabriel';
+const x = true;
+const anotherName = 1;
+
+function greeting(name: string){
+	console.log('Olá, ' + name);
+}
+
+greeting(firstname);
