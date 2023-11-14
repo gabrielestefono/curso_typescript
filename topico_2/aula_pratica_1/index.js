031a22c528dfc38a5da22de8cb8865ac0c67a804
gabrielestefono <gabrielestefono@hotmail.com>
2023-11-14 08:58:27 -0300

    Tipos number

---
+++ topico_2/aula_pratica_1/index.js
@@ -0,0 +1,6 @@
+var x = 10;
+console.log(x);
+var y = 15.5850;
+console.log(y);
+console.log(typeof x);
+console.log(typeof y);
---
+++ topico_2/aula_pratica_1/index.ts
@@ -0,0 +1,11 @@
+let x: number = 10;
+
+console.log(x);
+
+const y: number = 15.5850;
+
+console.log(y);
+
+console.log(typeof x)
+
+console.log(typeof y)
