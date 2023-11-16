baa04ad93a4b9c8de9b8ea79dc6d564d2703a91f
gabrielestefono <gabrielestefono@hotmail.com>
2023-11-16 08:37:05 -0300

    O tipo string

---
+++ topico_2/aula_pratica_2/index.js
@@ -0,0 +1,6 @@
+var firstname = 'Gabriel';
+console.log(firstname);
+var fullname;
+var lastname = "Estéfono";
+fullname = firstname + ' ' + lastname;
+console.log(fullname);
---
+++ topico_2/aula_pratica_2/index.ts
@@ -0,0 +1,11 @@
+const firstname: string = 'Gabriel';
+
+console.log(firstname)
+
+let fullname: string;
+
+const lastname: string = "Estéfono";
+
+fullname = firstname + ' ' + lastname;
+
+console.log(fullname);
