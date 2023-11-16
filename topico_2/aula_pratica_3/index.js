8574db397ca4d3475c4b5e1bdd37a5ec1a0524dd
gabrielestefono <gabrielestefono@hotmail.com>
2023-11-16 08:41:55 -0300

    Tipo boolean

---
+++ topico_2/aula_pratica_3/index.js
@@ -0,0 +1,5 @@
+var a = true;
+console.log(a);
+console.log(typeof (a));
+a = false;
+console.log(a);
---
+++ topico_2/aula_pratica_3/index.ts
@@ -0,0 +1,9 @@
+let a:boolean = true;
+
+console.log(a);
+
+console.log(typeof(a));
+
+a = false;
+
+console.log(a);
