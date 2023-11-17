c3ee4dad5bb692bc5946bc2a1601a410e28dfe19
gabrielestefono <gabrielestefono@hotmail.com>
2023-11-17 08:50:13 -0300

    Arrays 02

---
+++ topico_03/aula_pratica_02/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let nums = [100, 200];
+nums.push(300);
+console.log(nums);
+console.log(nums[2]);
---
+++ topico_03/aula_pratica_02/index.ts
@@ -0,0 +1,7 @@
+let nums: Array<number> = [100,200];
+
+nums.push(300);
+
+console.log(nums);
+
+console.log(nums[2])
