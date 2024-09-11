529305a9f9348a8e43a862a914bdf6c2f073059d
gabrielestefono <gabrielestefono@hotmail.com>
2024-09-11 06:25:45 -0300

    Tipo de Retorno

---
+++ topico_03/aula_pratica_05/index.js
@@ -0,0 +1,4 @@
+function greeting(name) {
+    return "Ol\u00E1 ".concat(name);
+}
+console.log(greeting("Gabriel"));
---
+++ topico_03/aula_pratica_05/index.ts
@@ -0,0 +1,5 @@
+function greeting(name: string): string {
+    return `Olá ${name}`
+}
+
+console.log(greeting("Gabriel"))
