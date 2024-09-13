dc37687e4c4fb07013b2c2d9fefe9bc6cb1ac1e8
gabrielestefono <gabrielestefono@hotmail.com>
2024-09-13 05:52:35 -0300

    Literal Types

---
+++ topico_03/aula_pratica_15/index.js
@@ -0,0 +1,9 @@
+var test;
+test = "testando";
+console.log(test);
+function showDirection(direction) {
+    console.log("A dire\u00E7\u00E3o \u00E9: ".concat(direction));
+}
+showDirection("center");
+showDirection("left");
+showDirection("right");
---
+++ topico_03/aula_pratica_15/index.ts
@@ -0,0 +1,13 @@
+let test: "testando";
+
+test = "testando";
+
+console.log(test)
+
+function showDirection(direction: "left" | "right" | "center") {
+    console.log(`A direção é: ${direction}`);
+}
+
+showDirection("center")
+showDirection("left")
+showDirection("right")
