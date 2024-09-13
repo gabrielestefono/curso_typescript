85545558eb2f323ed3e1917ce7421d054ac9a9d1
gabrielestefono <gabrielestefono@hotmail.com>
2024-09-13 05:36:12 -0300

    Type Alias

---
+++ topico_03/aula_pratica_12/index.js
@@ -0,0 +1,5 @@
+function showId(id) {
+    console.log("O id \u00E9 ".concat(id));
+}
+showId(1);
+showId("200");
---
+++ topico_03/aula_pratica_12/index.ts
@@ -0,0 +1,8 @@
+type ID = string | number;
+
+function showId(id: ID){
+    console.log(`O id é ${id}`)
+}
+
+showId(1)
+showId("200")
