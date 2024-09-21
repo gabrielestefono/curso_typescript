1ee8d3dd08e0c5c326f712b23c266a65f4124b19
gabrielestefono <gabrielestefono@hotmail.com>
2024-09-20 22:10:03 -0300

    Iniciando mobile do projeto reciple

--- projeto_estudos/src/pages/admin/index.tsx
+++
@@ -1,5 +0,0 @@
-import Layout from "@/layout";
-
-export default function Admin() {
-  return <Layout>Oi</Layout>;
-}
--- projetos/front/recipe-page/src/pages/index.tsx
+++ projetos/front/recipe-page/src/pages/index.tsx
@@ -1,8 +1,15 @@
+import styles from "../styles/Home.module.scss";
+
 export default function Home() {
   return (
-    <main>
+    <main className={styles.home}>
       <div>
-        <img src="" alt="" width={375} height={170}/>
+        <img
+          src="/images/image-omelette.jpeg"
+          alt=""
+          style={{ width: "100%", height: "300px", maxHeight: "26vh" }}
+        />
+        <p>Simple Omelette Recipe</p>
       </div>
     </main>
   );
