fc02f21f934d0aeaf668b477fea81892e2e832d3
gabrielestefono <gabrielestefono@hotmail.com>
2024-09-30 18:23:09 -0300

    feat: :mag: Melhorando acessibilidade e SEO

--- projetos/front/Newbie/02 - Social links profile/src/pages/_document.tsx
+++ projetos/front/Newbie/02 - Social links profile/src/pages/_document.tsx
@@ -3,7 +3,34 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Frontend Developer - Jessica Randall</title>
+        <meta
+          name="description"
+          content="Explore o portfólio de Jessica Randall, uma desenvolvedora frontend especializada em design responsivo e soluções criativas."
+        />
+        <meta
+          name="keywords"
+          content="desenvolvedora frontend, portfólio, Jessica Randall, design responsivo, HTML, CSS, JavaScript"
+        />
+        <meta name="author" content="Jessica Randall" />
+        <link rel="canonical" href="http://localhost:3000/" />
+        <meta
+          property="og:title"
+          content="Frontend Developer - Jessica Randall"
+        />
+        <meta
+          property="og:description"
+          content="Explore o portfólio de Jessica Randall, uma desenvolvedora frontend."
+        />
+        <meta property="og:url" content="http://localhost:3000/" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="http://localhost:3000/images/avatar-jessica.jpeg"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
